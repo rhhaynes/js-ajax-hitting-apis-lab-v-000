acf360b8711f0694728a2194288e9b4ad999a783
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 15:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function showRepositories(event, data) {
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
+  req.open("GET", 'https://api.github.com/users/octocat/repos');
+  req.send();
 }
