1db15296b5928ea70186f1bfdcb666ccc7114f94
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 16:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function displayBranches(){
 
 function displayCommits(){
   const commits = JSON.parse(this.responseText);
-  debugger;
   const commitsList = '<ul>' + commits.map(commit =>
     `<li><strong>${commit.author.login}</strong> - ${commit.commit.message}</li>`
     ).join('') + '</ul>';
