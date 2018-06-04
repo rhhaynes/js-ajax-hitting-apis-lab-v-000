c3b626998f5af9aa83a7ace28a1ee1f58fadca1e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 15:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   let repos = JSON.parse(this.responseText);
   const repoList = '<ul>'+repos.map(r => `<li>${r.name} - <a href="#" data-repo="${r.name}" onclick="getCommits(this)">Get Commits</a></li>`).join('')+'</ul>';
   document.getElementById("repositories").innerHTML = repoList;
