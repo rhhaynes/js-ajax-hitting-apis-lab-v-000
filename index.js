7026ccd2fa06d2f8bb503843390cb9fab4427fa4
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 16:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function displayRepositories(event, data) {
 function displayBranches(){}
 
 function displayCommits(){}
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
 
 function getRepositories() {
   const req  = new XMLHttpRequest();
