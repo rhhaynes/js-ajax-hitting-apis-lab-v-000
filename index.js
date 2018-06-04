6ddb6021ce473ad12de3b77d0fc79331ab3cdedd
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 15:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function showRepositories(event, data) {
 
 function getRepositories() {
   const req  = new XMLHttpRequest();
-  const user = getElementById('username');
+  const user = document.getElementById('username');
   debugger;
   req.addEventListener("load", showRepositories);
   req.open("GET", `https://api.github.com/users/${user}/repos`);
