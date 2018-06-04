15d5b758d800ef82c5ab5c92a5a0da3e1e1bb7e0
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 17:08:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener("load", displayCommits);
-  req.open("GET", `https://api.github.com/users/${user}/repos`);
+  req.open("GET", `https://api.github.com/repos/${user}/${}/commits`);
   req.send();
 }
