f243e17e9f8960e1ba64056c2438b24110508e00
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-04 17:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function displayRepositories(event, data) {
 
 function displayBranches(){
   const branches = JSON.parse(this.responseText);
-  const branchesList = '<strong>Branches:</strong><ol>' +
-    branches.map(branch => `<li>${branch.name}</li>`).join('') + '</ol>';
+  const branchesList = '<strong>Branches:</strong><ul>' +
+    branches.map(branch => `<li>${branch.name}</li>`).join('') + '</ul>';
   document.getElementById('details').innerHTML = branchesList;
 }
 
